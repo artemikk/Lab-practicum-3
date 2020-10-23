7330f9ecbcdba3eb1b4be2c9cfa1de8cc0cab54e
Артём <@DESKTOP-DJS4ML8>
2020-10-23 23:33:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        List<student> studGroup = new List<student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            student a1 = new student(111, "Иванов Саша");
+            student a2 = new student(222, "Петров Витя");
+            student a3 = new student(333, "Васильева Юля");
+            student a4 = new student(444, "Гришин Костя");
+            studGroup.Add(a1);
+            studGroup.Add(a2);
+            studGroup.Add(a3);
+            studGroup.Add(a4);
+
+            listBox1.DisplayMember = "name";  // будет показываться свойство Name
+            listBox1.ValueMember = "id";  // Будет возвращаться значение свойства ID
+            listBox1.DataSource = studGroup;  // задана коллекция
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                int n = listBox1.SelectedIndex;
+                double b = (double)listBox1.SelectedValue;
+                MessageBox.Show("ID студента = " + b.ToString());
+        }
+    }
+}
---
+++ WindowsFormsApp2/student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class student
+    {
+        public double id { get; set; }
+        public string name{ get; set; }
+        public student()
+        {
+        }
+        public student(double nomer, string fio)
+        {
+            id = nomer;
+            name = fio;
+        }
+    }
+}
